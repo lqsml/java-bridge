f82c773cc512d4326711aff377d884db4b800d1f
lqsml <dvlprsml@gmail.com>
2022-11-21 19:18:56 +0900

    feat: 유저 입력 페이지(InputView.java) 1) 다리 길이 입력 2) 이동할 칸 입력 3) 게임 재시도 여부 입력

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,28 +1,39 @@
 package bridge;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
-
+	
+	static final String INPUT_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+	static final String INPUT_MOVE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+	static final String INPUT_RETRY_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+    	System.out.println(INPUT_SIZE_MESSAGE);
+    	int inputSize = Integer.parseInt(readLine().replace(" ", ""));
+        return inputSize;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+    	System.out.println(INPUT_MOVE_MESSAGE);
+    	String inputMoving = readLine().replace(" ", "");
+        return inputMoving;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+    	System.out.println(INPUT_RETRY_MESSAGE);
+    	String inputCommand = readLine().replace(" ", "");
+        return inputCommand;
     }
 }
